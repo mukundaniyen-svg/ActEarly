8e1c60819663a18fe676cf26c1f4156e705f5f0b
Pooja Sudarsanam <poojasudarsanam@Poojas-MacBook-Pro.local>
2026-04-02 15:34:46 +0530

    Adding UserID for Firebase

---
+++ src/lib/user.js
@@ -0,0 +1,10 @@
+export const getOrCreateUserId = () => {
+  let userId = localStorage.getItem("user_id");
+
+  if (!userId) {
+    userId = crypto.randomUUID();
+    localStorage.setItem("user_id", userId);
+  }
+
+  return userId;
+};
